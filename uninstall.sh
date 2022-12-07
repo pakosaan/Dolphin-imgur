26ca937d50f9e3b153bda8107139c1105c4f67df
Utshab Pyakurel <pyakurelutshab@gmail.com>
2022-12-07 11:59:14 +0545

    changes the path in imgur and uninstall.sh file

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm "`kf5-config --path services | cut -f1 -d':'`imgur.desktop"
-[ -e "$HOME/.imgur_history" ] && rm $HOME/.imgur_history
 sudo rm /usr/share/pixmaps/imgur.png
-sudo rm $HOME/.local/bin/imgur
+sudo rm /usr/bin/imgur
